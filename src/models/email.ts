9b85c956f3a79a3a74b60bff81af7aa778521fdf
BrendoKatagi <72100371+BrendoKatagi@users.noreply.github.com>
2022-09-02 19:40:38 -0300

    feat: add email regex

--- src/models/email.ts
+++ src/models/email.ts
@@ -13,6 +13,11 @@ class Email {
     if(email.length > 320) 
       return false;
 
+    const emailRegex = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+    
+    if(!emailRegex.test(email))
+      return false;
+    
     const [username, domain] = email.split('@');
     if(username.length > 64 || domain.length > 255)
       return false;
