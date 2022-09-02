96b1599c7cfdffba7a84e70333da952e95acd773
BrendoKatagi <72100371+BrendoKatagi@users.noreply.github.com>
2022-09-02 19:03:44 -0300

    test: add email validation for emails with more than 320 chars

--- test/models/email-validation.spec.ts
+++ test/models/email-validation.spec.ts
@@ -4,4 +4,10 @@ describe('Email validation', () => {
   test('should not accept empty email', () => {
     expect(Email.validate(null)).toBeFalsy();
   });
+
+  test('should return false for emails bigger than 320 chars', () => {
+    let address = 'a';
+    expect(Email.validate(address.repeat(310) + '@emails.com')).toBeFalsy();
+  });
+
 })
