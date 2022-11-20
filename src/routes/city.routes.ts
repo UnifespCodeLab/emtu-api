295b9f34d29ca6208e317e77764c69602d3d4353
BrendoKatagi <72100371+BrendoKatagi@users.noreply.github.com>
2022-11-20 16:33:37 -0300

    fix: add city controller in city routes

--- src/routes/city.routes.ts
+++ src/routes/city.routes.ts
@@ -1,23 +1,9 @@
 import { Router } from "express";
-import { Request, Response } from "express";
-import { PostgresCityDataSource } from "../database/db/city/postgresCityDataSource";
-import CityRepository from "../models/repositories/implementations/CityRepository";
-import { GetAllCitiesUseCase } from "../models/useCases/city/getAllCities";
-
-const cityDataSource = new PostgresCityDataSource();
-const cityRepository = new CityRepository(cityDataSource);
-const getAllCitiesUseCase = new GetAllCitiesUseCase(cityRepository);
+import cityController from './../controllers/cityController';
 
 const cityRoutes = Router();
 
-cityRoutes.get('/', async (req: Request, res: Response) => {
-  try {
-    const cities = await getAllCitiesUseCase.execute();
-    res.status(200).send(cities);
-  } catch (error) {
-    res.status(500).send({ mensagem: "Erro ao obter dados" });
-  }
-});
+cityRoutes.get('/', cityController.getAllCities);
 
 export { cityRoutes }
 
