e01925f572bc6b674122ff395795473e49b20480
BrendoKatagi <72100371+BrendoKatagi@users.noreply.github.com>
2022-12-07 23:00:00 -0300

    feat: create database methods to retrieve user data or insert a new user

---
+++ src/database/db/user/postgresUserDataSource.ts
@@ -0,0 +1,27 @@
+import { Pool, QueryResult } from "pg";
+import { UserDto } from "../../../dtos/userDto";
+import User from "../../../models/user";
+import IUserDataSource from "../../interfaces/userDataSource";
+import PostgresDB from "../postgresDB";
+
+export class PostgresUserDataSource implements IUserDataSource {
+  private dataBase: Pool;
+
+  constructor(){
+    this.dataBase = PostgresDB.getInstance();
+  }
+
+  async getByEmail(email: string): Promise<User> {
+    const result = await this.dataBase.query(`SELECT * FROM users u WHERE u.email = '${email}';`);
+    return PostgresUserDataSource.mapResultToModel(result)[0];
+  }
+
+  async create(user : User) : Promise<QueryResult> {
+    const id = await this.dataBase.query(`INSERT INTO users(name, email, password) VALUES ('${user.name}', '${user.email}', '${user.password}') RETURNING *`);
+    return id;
+  };
+  
+  private static mapResultToModel = (result: QueryResult<UserDto>): User[] => result.rows.map(
+    (row) => (new User(row.name, row.email, row.password))
+  );
+} 
