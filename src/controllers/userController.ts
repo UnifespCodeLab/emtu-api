036c83b06a4984ba287e1f820e38188e7c809cf2
BrendoKatagi <72100371+BrendoKatagi@users.noreply.github.com>
2022-12-12 21:20:07 -0300

    config: create .env.sample

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,7 +1,7 @@
 import {Request, Response} from "express";
 import PasswordBcrypt from "../core/passwordBcrypt";
+import tokenJWT from "../core/tokenJWT";
 import { PostgresUserDataSource } from "../database/db/user/postgresUserDataSource";
-import jwt from "jsonwebtoken";
 import User from "../models/user";
 
 const userDataSource = new PostgresUserDataSource();
@@ -30,13 +30,15 @@ export default class UserController {
 
       const user = new User(name, email, encryptedPassword);
 
-      const userData = await userDataSource.create(user);
+      const createUserSuccess = await userDataSource.create(user);
 
-      const token = jwt.sign(userData, 
-        process.env.JWT_SECRET_KEY,
-        {
-          expiresIn: "2h",
-        });
+      if(!createUserSuccess) return res.status(500).send({erro:"Falha ao cadastrar usuário"});
+
+      const token = tokenJWT.create({
+        "name": user.name,
+        "email": user.email,
+        "password": user.password
+      });
 
       res.status(201).json({
         token: token
@@ -47,4 +49,45 @@ export default class UserController {
       });
     }
   }
+
+  public static async login(req: Request, res: Response): Promise<Response> {
+    try {
+      const { email, password } = req.body;
+
+      if(!(email && password)){
+        return res.status(400).send({
+          erro:"Não foram enviados todos os parâmetros obrigatórios"
+        });
+      }
+
+      const user  = await userDataSource.getByEmail(email);
+      const isPasswordValid = user ? 
+        await passwordEncrypter.isValid(password, user.password) 
+        : null;
+
+      if(!user || !isPasswordValid) {
+        return res.status(400).send({
+          erro:"Parâmetros inválidos"
+        });
+      };
+      
+      const userData = {
+        "name": user.name,
+        "email": user.email,
+        "password": user.password,
+      };
+
+      const token = tokenJWT.create(userData);
+
+      return res.status(201).json({
+        token: token
+      });
+      
+    } catch (error) {
+      return res.status(500).send({
+        erro:"Falha ao efetuar o login do usuário"
+      });
+    }
+  }
+
 }
