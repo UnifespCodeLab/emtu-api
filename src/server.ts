03631986b3c6939d5c50f161d0a3b8379af9177f
DanielSalis <danielsalis98@gmail.com>
2023-01-22 14:07:28 -0300

    feat: import cors into server

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 import swaggerUi from 'swagger-ui-express';
 import swaggerFile from './swagger.json';
+import cors from 'cors'
 
 import { router } from './routes';
 
 const app = express();
 
 app.use(express.json());
+app.use(cors())
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
