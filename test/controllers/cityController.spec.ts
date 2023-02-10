5f9f077f50ca6f86d1ba095bd76da8acd79c40d2
BrendoKatagi <72100371+BrendoKatagi@users.noreply.github.com>
2023-02-10 00:55:35 -0300

    test: should return 400 when passing an invalid id

---
+++ test/controllers/cityController.spec.ts
@@ -0,0 +1,15 @@
+import request from 'supertest';
+import cityController from "../../src/controllers/cityController";
+import express from "express";
+
+const cityRoutes = express();
+
+cityRoutes.get('/', cityController.getAllCities);
+cityRoutes.get('/:idCity', cityController.getCityById);
+
+describe("get city by id", () => {
+  test("should return 400 when passing an invalid id", async() => {
+    const res = await request(cityRoutes).get('/0');
+    expect(res.statusCode).toBe(400);
+  })
+});
