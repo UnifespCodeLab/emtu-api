39db501271fc2ab6d4ffc45bee161c9c31765e74
BrendoKatagi <72100371+BrendoKatagi@users.noreply.github.com>
2023-02-10 00:40:46 -0300

    feat: create new route getCityById

--- src/routes/city.routes.ts
+++ src/routes/city.routes.ts
@@ -4,6 +4,7 @@ import cityController from './../controllers/cityController';
 const cityRoutes = Router();
 
 cityRoutes.get('/', cityController.getAllCities);
+cityRoutes.get('/:idCity', cityController.getCityById);
 
 export { cityRoutes }
 
