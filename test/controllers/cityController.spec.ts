c1990dc57b5e00d270419e61499c947e15a0e67c
BrendoKatagi <72100371+BrendoKatagi@users.noreply.github.com>
2023-02-10 01:17:56 -0300

    test: should return erro message when passing an invalid id

--- test/controllers/cityController.spec.ts
+++ test/controllers/cityController.spec.ts
@@ -13,8 +13,14 @@ describe("get city by id", () => {
     expect(res.statusCode).toBe(400);
   });
 
+  test("should an error message when passing an invalid id", async() => {
+    const res = await request(cityRoutes).get('/0');
+    expect(JSON.parse(res.text).erro).toBe("ID inválido");
+  });
+
   test("should return 200 when passing an valid id", async() => {
     const res = await request(cityRoutes).get('/1');
     expect(res.statusCode).toBe(200);
   });
+
 });
