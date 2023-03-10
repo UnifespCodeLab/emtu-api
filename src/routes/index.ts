8dd6b8cdd7c69ff7d507ca39a0125deaf26f4400
BrendoKatagi <72100371+BrendoKatagi@users.noreply.github.com>
2023-03-10 01:00:25 -0300

    feat: create bus routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import { cityRoutes } from "./city.routes";
 import { cidsRoutes } from "./cids.routes";
 import { userRoutes } from "./user.routes";
+import { busRoutes } from './bus.routes';
 
 const router = Router();
 
 router.use("/city", cityRoutes);
 router.use("/cids", cidsRoutes);
 router.use("/user", userRoutes);
+router.use("/bus", busRoutes);
 
 export { router }
