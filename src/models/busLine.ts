067b2843dceab9419f22a4d5bad03216a826e506
BrendoKatagi <72100371+BrendoKatagi@users.noreply.github.com>
2023-03-22 02:03:16 -0300

    feat: created busLine class

---
+++ src/models/busLine.ts
@@ -0,0 +1,39 @@
+
+export default class BusLine {
+  codigo: string;
+  cidade_origem: string;
+  cidade_destino: string;
+  horarios: Date[];
+  horariosdiasuteis: Date[];
+  horariossabados: Date[];
+  horariosdomingosferiados: Date[];
+
+  constructor(codigo: string,cidade_origem: string,cidade_destino: string,horarios: Date[],horariosdiasuteis: Date[],horariossabados: Date[],horariosdomingosferiados: Date[]){
+    this.codigo = codigo;
+    this.cidade_origem = cidade_origem;
+    this.cidade_destino = cidade_destino;
+    this.horarios = horarios;
+    this.horariosdiasuteis = horariosdiasuteis;
+    this.horariossabados = horariossabados;
+    this.horariosdomingosferiados = horariosdomingosferiados;
+  }
+
+  static fromJson(obj: any): BusLine[]{
+    let lineDetails: BusLine[] = [];
+    obj.map(route => {
+      let busLine = new BusLine(
+        route.data.linhas[0].codigo,
+        route.data.linhas[0].rotas[0].cidade,
+        route.data.linhas[0].rotas[0].destino,
+        route.data.linhas[0].rotas[0].horarios.split(',').map(hour => new Date('1/1/1999 ' + hour)),
+        route.data.linhas[0].rotas[0].horariosdiasuteis.split(',').map(hour => new Date('1/1/1999 ' + hour)),
+        route.data.linhas[0].rotas[0].horariossabados.split(',').map(hour => new Date('1/1/1999 ' + hour)),
+        route.data.linhas[0].rotas[0].horariosdomingosferiados.split(',').map(hour => new Date('1/1/1999 ' + hour)),
+      );
+
+      lineDetails.push(busLine);
+    });
+
+    return lineDetails;
+  }
+}
