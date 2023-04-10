93e69a6bd4fc3c49f77f508f5149a5c094785d13
Thiago Henrique Leite da Silva <thiago.leitesilva05@gmail.com>
2023-04-10 19:47:05 -0300

    feat(reports): add column data_criacao to reports table

--- scripts/reports/create.sql
+++ scripts/reports/create.sql
@@ -10,6 +10,7 @@ CREATE TABLE public.reports (
   id_cidade_origem INT NOT NULL,
   id_cidade_destino INT NOT NULL,
   id_cid INT NOT NULL,
+  data_criacao TIMESTAMP NOT NULL,
   CONSTRAINT reports_pkey PRIMARY KEY (id),
   FOREIGN KEY (id_cidade_origem) REFERENCES city(id),
   FOREIGN KEY (id_cidade_destino) REFERENCES city(id),
