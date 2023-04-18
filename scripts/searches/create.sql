446263df0e0ba8718d615a3fa7501b0b2bdee8c6
Thiago Henrique Leite da Silva <thiago.leitesilva05@gmail.com>
2023-04-18 12:37:22 -0300

    fix(searches): make column id_cid optional

--- scripts/searches/create.sql
+++ scripts/searches/create.sql
@@ -8,7 +8,7 @@ CREATE TABLE public.searches (
   id serial4 NOT NULL,
   id_cidade_origem INT NOT NULL,
   id_cidade_destino INT NOT NULL,
-  id_cid INT NOT NULL,
+  id_cid INT,
   data_viagem DATE,
   hora_viagem VARCHAR(255),
   data_criacao TIMESTAMP NOT NULL,
