2eba4a495869828cf0d9e180882c5615a018445d
Milena Siqueira <milematoss1@gmail.com>
2023-09-17 18:15:53 -0300

    feat: save the search after fetching routes

--- src/controllers/busController.ts
+++ src/controllers/busController.ts
@@ -14,6 +14,9 @@ import { PostgresVehicleDatasource } from './../database/db/vehicle/postgresVehi
 import CidsRepository from "../models/repositories/implementations/CidsRepository";
 import { PostgresCidsDataSource } from "../database/db/cids/potsgresCidsDataSource";
 import { BusLineDto } from "../dtos/busLineDto";
+import { RouteSearchDto } from "../dtos/routeSearchDto";
+import { PostgresRouteSearchDataSource } from "../database/db/routeSearch/postgresRouteSearchDataSource";
+import BusRoute from "../models/busRoute";
 
 const cityDataSource = new PostgresCityDataSource();
 const cityRepository = new CityRepository(cityDataSource);
@@ -28,27 +31,30 @@ const vehicleRepository = new VehicleRepository(vehicleDatasource);
 const cidsDatasource = new PostgresCidsDataSource();
 const cidsRepository = new CidsRepository(cidsDatasource);
 const verifyAccessibilityUseCase = new VerifyAccessibilityUseCase(vehicleRepository, cidsRepository);
+const routeSearchDataSource = new PostgresRouteSearchDataSource();
 
 export default class busController {
 
-  public static async getRoutes(req:Request<busRouteDTO>, res:Response) {
+  private static async getBusRoutes(body) {
     try {
-      if(!req.body || Object.keys(req.body).length === 0)
-        return res.status(400).send({erro: "Objeto enviado é inválido"});
+      if(!body || Object.keys(body).length === 0)
+        throw({status: 400, message: "Objeto enviado é inválido"})
   
-      const { originCityId, destinationCityId, data, hora, cid } = req.body;
+      const { originCityId, destinationCityId, data, hora, cid } = body;
       if(!originCityId || !destinationCityId)
-        return res.status(400).send({erro: "Cidade de origem ou destino inválidos"});
+        throw({status: 400, message: "Cidade de origem ou destino inválidos"})
+
       if(originCityId < 1 || originCityId > 134 || destinationCityId < 1 || destinationCityId > 134)
-        return res.status(400).send({erro: "Cidade de origem ou destino inválidos"});
-      if(!cid || cid <= 0 )
-        return res.status(400).send({erro: "CID inválido"});
+        throw({status: 400, message: "Cidade de origem ou destino inválidos"})
+      
+       if(!cid || cid <= 0)
+        throw({status: 400, message: "CID inválido"})
 
       let originCity = await getCityByIdUseCase.execute(originCityId);
       let destinyCity = await getCityByIdUseCase.execute(destinationCityId);
 
       if(!originCity || !destinyCity)
-        return res.status(400).send({erro: "Rota não encontrada para as cidades informadas"});
+        throw({status: 400, message: "Rota não encontrada para as cidades informadas"})
 
       let routeIds = await getBusRoutesUseCase.execute(originCity.getName(), destinyCity.getName());
       
@@ -60,11 +66,57 @@ export default class busController {
         );    
 
       await busController.checkRoutesAccessibility(lines, cid);
-      return res.status(200).send(lines);
+      return {routeIds, lines}
     } catch (error) {
-      console.log(error)
-      return res.status(500).send({erro: "Ocorreu um erro ao obter a linha informada"});
+      if (error.status) {
+        throw(error)
+      } else {
+        console.log(error)
+        throw({status: 500, message: "Ocorreu um erro ao obter a linha informada"})
+      }
     }
+  }
+
+  static async saveRouteSearch(body, routes:BusRoute[], lines:BusLineDto[]) {
+    const { originCityId, destinationCityId, data, hora, cid } = body;
+    
+    let searchBody: RouteSearchDto[] = [];
+    routes.forEach(route => searchBody.push({
+      idCidadeOrigem: originCityId,
+      idCidadeDestino: destinationCityId,
+      idCid: cid,
+      idLinha: route.routeShortName,
+      sucedida: Boolean(lines.find(line => line.code == route.routeShortName)),
+      dataViagem: data,
+      horaViagem: hora,
+      dataCriacao: new Date()
+    }))
+
+    try {
+      searchBody.forEach(async item => 
+        await routeSearchDataSource.create(item)
+      )
+  
+    } catch(error) {
+      console.log("Erro ao salvar busca:", error)
+    }
+  }
+
+  public static async getRoutes(req:Request<busRouteDTO>, res:Response) {
+    let routeIds, lines
+    
+    try {
+      const result = await busController.getBusRoutes(req.body)
+      routeIds = result.routeIds
+      lines = result.lines
+    }
+    catch(error) {
+      return res.status(error.status).send({erro: error.message});
+    }
+
+    await busController.saveRouteSearch(req.body, routeIds, lines)
+
+    return res.status(200).send(lines);
   }
 
   static async checkRoutesAccessibility(lines: BusLineDto[], cid: number) : Promise<void> {
@@ -72,7 +124,6 @@ export default class busController {
     for(linePos=0; linePos < lines.length; linePos++) {
       let line = lines[linePos];
       line.vehicle = await verifyAccessibilityUseCase.execute(line.prefix, cid);
-
     } 
   }
 }
