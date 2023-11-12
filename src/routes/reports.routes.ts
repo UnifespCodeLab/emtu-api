59d98e7f9557e2fabcefb329a961747290cdfe56
DanielSalis <danielsalis98@gmail.com>
2023-11-12 02:01:37 -0300

    feat: create dtos and add new routes

--- src/routes/reports.routes.ts
+++ src/routes/reports.routes.ts
@@ -4,5 +4,6 @@ import reportsController from './../controllers/reportsController';
 const reportsRoutes = Router();
 
 reportsRoutes.post("/", reportsController.create);
+reportsRoutes.get("/getReports", reportsController.getReports);
 
 export { reportsRoutes }
