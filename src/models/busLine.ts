d8ac0537667d4b71d6ebf4fac2a8b629b2ae1f4b
DanielSalis <danielsalis98@gmail.com>
2023-12-04 22:10:51 -0300

    fix: bus routes empty lines

--- src/models/busLine.ts
+++ src/models/busLine.ts
@@ -30,24 +30,25 @@ export default class BusLine {
   }
 
   static fromJson(obj: any): BusLine[]{
+
     let lineDetails: BusLine[] = [];
-    obj.map(route => {
-      let lines = route.data.linhas[0];
-      let lineRoutes = lines.rotas[0];
+    obj.filter(route=> route.data.linhas.length > 0).map(route => {
+        let lines = route.data.linhas[0];
+        let lineRoutes = lines.rotas[0];
 
-      let busLine = new BusLine(
-        lines.codigo,
-        lineRoutes.cidade,
-        lineRoutes.destino,
-        lineRoutes.horarios.split(',').map(hour => new Date('1/1/1999 ' + hour)),
-        lineRoutes.pontos,
-        lineRoutes.horariosdiasuteis.split(',').map(hour => new Date('1/1/1999 ' + hour)),
-        lineRoutes.horariossabados.split(',').map(hour => new Date('1/1/1999 ' + hour)),
-        lineRoutes.horariosdomingosferiados.split(',').map(hour => new Date('1/1/1999 ' + hour)),
-        lines.veiculos.map(veiculo => veiculo.prefixo)
-        );
+        let busLine = new BusLine(
+          lines.codigo,
+          lineRoutes.cidade,
+          lineRoutes.destino,
+          lineRoutes.horarios.split(',').map(hour => new Date('1/1/1999 ' + hour)),
+          lineRoutes.pontos,
+          lineRoutes.horariosdiasuteis.split(',').map(hour => new Date('1/1/1999 ' + hour)),
+          lineRoutes.horariossabados.split(',').map(hour => new Date('1/1/1999 ' + hour)),
+          lineRoutes.horariosdomingosferiados.split(',').map(hour => new Date('1/1/1999 ' + hour)),
+          lines.veiculos.map(veiculo => veiculo.prefixo)
+          );
 
-      lineDetails.push(busLine);
+        lineDetails.push(busLine);
     });
 
     return lineDetails;
