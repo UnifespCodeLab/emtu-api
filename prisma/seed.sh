d3dbd5595d2b31459e6cb213009ee50b4538b4d0
Enzo0100 <enzo.ceravolo@unifesp.br>
2025-07-09 13:00:47 -0300

    add seed user

--- prisma/seed.sh
+++ prisma/seed.sh
@@ -3,8 +3,8 @@
 # -x Print commands before they are executed
 set -ex
 # Seeding command
-psql -U <NOME_USUARIO_DA_ENV> -d <NOME_DB> -a -f  ../scripts/city/cidade.sql &&
-psql -U <NOME_USUARIO_DA_ENV> -d <NOME_DB> -a -f  ../scripts/cids/cids_202212242142.sql &&
-psql -U <NOME_USUARIO_DA_ENV> -d <NOME_DB> -a -f  ../scripts/group/group__202212251022.sql &&
-psql -U <NOME_USUARIO_DA_ENV> -d <NOME_DB> -a -f  ../scripts/routes/routes.sql &&
-psql -U <NOME_USUARIO_DA_ENV> -d <NOME_DB> -a -f  ../scripts/vehicles/vehicles_insert.sql
+psql -U postgres -d emtu -a -f  ../scripts/city/cidade.sql &&
+psql -U postgres -d emtu -a -f  ../scripts/cids/cids_202212242142.sql &&
+psql -U postgres -d emtu -a -f  ../scripts/group/group__202212251022.sql &&
+psql -U postgres -d emtu -a -f  ../scripts/routes/routes.sql &&
+psql -U postgres -d emtu -a -f  ../scripts/vehicles/vehicles_insert.sql
